0c42ad8565c7079a161b4547e4f6370c9b86efa8
Naoya Sogabe <naoyasu.ousia@gmail.com>
2025-12-22 00:06:44 +0900

    難読化

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,44 @@
 import { defineConfig } from 'vite';
+import obfuscator from 'rollup-plugin-obfuscator';
 
 export default defineConfig({
+  plugins: [
+    obfuscator({
+      global: true,
+      options: {
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 0.75,
+        deadCodeInjection: true,
+        deadCodeInjectionThreshold: 0.4,
+        debugProtection: false,
+        debugProtectionInterval: 0,
+        disableConsoleOutput: true,
+        identifierNamesGenerator: 'hexadecimal',
+        log: false,
+        numbersToExpressions: true,
+        renameGlobals: false,
+        selfDefending: true,
+        simplify: true,
+        splitStrings: true,
+        splitStringsChunkLength: 10,
+        stringArray: true,
+        stringArrayCallsTransform: true,
+        stringArrayCallsTransformThreshold: 0.75,
+        stringArrayEncoding: ['rc4'],
+        stringArrayIndexShift: true,
+        stringArrayRotate: true,
+        stringArrayShuffle: true,
+        stringArrayWrappersCount: 2,
+        stringArrayWrappersChainedCalls: true,
+        stringArrayWrappersParametersMaxCount: 4,
+        stringArrayWrappersType: 'function',
+        stringArrayThreshold: 0.75,
+        transformObjectKeys: true,
+        unicodeEscapeSequence: false
+      },
+    }),
+  ],
   base: './', // GitHub Pages用。リポジトリ名が決まっている場合は '/repo-name/' でも良い
   build: {
     outDir: 'dist',
