e16c9c4fcc8ffcb1a42edaa2832c6dadd3aca539
Naoya Sogabe <naoyasu.ousia@gmail.com>
2025-12-31 03:49:07 +0900

    最適化

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,9 +7,38 @@ const config: Config = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      // デザイントークン: A4サイズ定数
+      width: {
+        'a4': '210mm',
+      },
+      height: {
+        'a4': '297mm',
+      },
+      // デザイントークン: ページマージン
+      spacing: {
+        'page-margin': 'var(--page-margin, 17mm)',
+      },
+      // デザイントークン: パラグラフ番号の位置
+      inset: {
+        'para-number': '5mm',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    // scrollbar-hideユーティリティ
+    function({ addUtilities }: { addUtilities: any }) {
+      addUtilities({
+        '.scrollbar-hide': {
+          '-ms-overflow-style': 'none',
+          'scrollbar-width': 'none',
+          '&::-webkit-scrollbar': {
+            display: 'none',
+          },
+        },
+      });
+    },
+  ],
 };
 
 export default config;
